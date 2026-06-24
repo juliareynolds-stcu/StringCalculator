7d4e6d1934e071b8067c2295d71eac63a85bd7a1
Julia <julia.reynolds@stcu.org>
2026-06-24 16:52:04 -0700

    Add project files.

---
+++ StringCalculatorApp/StringCalculator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringCalculatorApp;
+
+/// <summary>
+/// Takes a string and calculates the sum of the string
+/// </summary>
+public class StringCalculator()
+{
+    /// <summary>
+    /// Sums ints contained in string. String must use commas (,) to delimit different ints.
+    /// </summary>
+    /// <param name="numbers">a string with ints separated by commas</param>
+    /// <returns>an integer representing the sum of the numbers in the string, 0 for a null or empty string</returns>
+    public int Add(string numbers)
+    {
+        if (string.IsNullOrWhiteSpace(numbers))
+        {
+            return 0;
+        }
+
+        var numArray = numbers.Split(",");
+        var parseIntErrors = 0;
+        var sum = 0;
+
+        foreach (string number in numArray)
+        {
+            if (int.TryParse(number.Trim(), out var toSum))
+            {
+                sum += toSum;
+            }
+            else
+            {
+                parseIntErrors++;
+            }
+        }
+
+        Console.WriteLine($"Integer parse errors:  {parseIntErrors}");
+
+        return sum;
+    }
+}
---
+++ StringCalculatorApp/StringCalculatorCLI.cs
@@ -0,0 +1,20 @@
+﻿namespace StringCalculatorApp;
+
+/// <summary>
+/// a command line interface for the string calculator
+/// </summary>
+public static class StringCalculatorCLI
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to String Calculator, a project following https://osherove.com/tdd-kata-1/");
+        Console.WriteLine("\r\nPlease enter a string to be summed following the format int,int,int.");
+        Console.Write("input:  ");
+
+        var userInput = Console.ReadLine();
+        var calculator = new StringCalculator();
+        var result = calculator.Add(userInput);
+
+        Console.WriteLine($"\r\nresult:  {result}");
+    }
+}
