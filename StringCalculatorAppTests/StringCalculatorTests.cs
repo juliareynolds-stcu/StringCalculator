70b3b642714840fe1acde8e06ac1400bfc221ae1
Julia <julia.reynolds@stcu.org>
2026-06-25 08:34:13 -0700

    added empty string test and invalid input test

--- StringCalculatorAppTests/StringCalculatorTests.cs
+++ StringCalculatorAppTests/StringCalculatorTests.cs
@@ -24,5 +24,34 @@ public class StringCalculatorTests
         result.Should().Be(6);
     }
 
+    [Test]
+    public void ReturnsZeroWhenGivenNullOrEmptyString()
+    {
+        // Arrange
+        var input = "";
+
+        // Act
+        var sut = new StringCalculator();    // sut = system under test
+        var result = sut.Add(input);
+
+        // Assert
+        result.Should().Be(0);
+    }
+
+    [Test]
+    public void IgnoresInvalidInputs()
+    {
+        // Arrange
+        var input = "a, b, 1, c";
+
+        // Act
+        var sut = new StringCalculator();
+        var result = sut.Add(input);
+
+        // Assert
+        result.Should().Be(1);
+        sut.getParseIntErrors().Should().Be(3);
+    }
+
     // write fail test
 }
